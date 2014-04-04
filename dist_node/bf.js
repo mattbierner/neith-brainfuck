ae2132d0565acd89d9cd8864bfce6e04b5d419ed
Matt Bierner <mattbierner@gmail.com>
2014-04-04 14:12:34 -0400

    Updated readme

--- dist_node/bf.js
+++ dist_node/bf.js
@@ -2,35 +2,37 @@
  * THIS FILE IS AUTO GENERATED from 'lib/bf.kep'
  * DO NOT EDIT
 */"use strict";
-var __o = require("bennu")["parse"],
-    rec = __o["rec"],
-    run = __o["run"],
-    eager = __o["eager"],
-    either = __o["either"],
-    next = __o["next"],
-    many = __o["many"],
-    __o0 = require("bennu")["lang"],
-    between = __o0["between"],
-    sepEndBy = __o0["sepEndBy"],
-    __o1 = require("bennu")["text"],
-    character = __o1["character"],
-    oneOf = __o1["oneOf"],
-    noneOf = __o1["noneOf"],
+var __o = require("akh")["base"],
+    sequence = __o["sequence"],
+    sequencea = __o["sequencea"],
+    State = require("akh")["state"],
+    ZipperT = require("zipper-m")["trans"]["zipper"],
+    __o0 = require("bennu")["parse"],
+    rec = __o0["rec"],
+    run = __o0["run"],
+    eager = __o0["eager"],
+    either = __o0["either"],
+    next = __o0["next"],
+    many = __o0["many"],
+    __o1 = require("bennu")["lang"],
+    between = __o1["between"],
+    sepEndBy = __o1["sepEndBy"],
+    __o2 = require("bennu")["text"],
+    character = __o2["character"],
+    oneOf = __o2["oneOf"],
+    noneOf = __o2["noneOf"],
     stream = require("nu-stream")["stream"],
     first = stream["first"],
     rest = stream["rest"],
     isEmpty = stream["isEmpty"],
     map = stream["map"],
     NIL = stream["NIL"],
-    __o2 = require("nu-stream")["gen"],
-    repeat = __o2["repeat"],
+    __o3 = require("nu-stream")["gen"],
+    repeat = __o3["repeat"],
     zipper = require("neith")["zipper"],
-    __o3 = require("neith")["list"],
-    listZipperIn = __o3["listZipperIn"],
-    evaluateStream, evaluate, parse, x, x0, eof, id = (function(x) {
-        return x;
-    }),
-    charCode = Function.prototype.call.bind(String.prototype.charCodeAt),
+    __o4 = require("neith")["list"],
+    listZipperIn = __o4["listZipperIn"],
+    evaluateStream, evaluate, parse, x, x0, eof, charCode = Function.prototype.call.bind(String.prototype.charCodeAt),
     fromCharCode = String.fromCharCode,
     op = oneOf("><+-.,"),
     other = many(noneOf("><+-.,[]")),
@@ -39,45 +41,31 @@ var __o = require("bennu")["parse"],
         return next(other, eager(sepEndBy(other, either(op, block(self)))));
     }));
 (parse = run.bind(null, program));
-var move = (function(op) {
-    return (function(z, i) {
-        return [op(z), i];
-    });
-}),
-    inc = move(zipper.modify.bind(null, ((x = 1), (function(y) {
+var M = ZipperT(State),
+    inc = M.move(zipper.modify.bind(null, ((x = 1), (function(y) {
         return (x + y);
     })))),
-    dec = move(zipper.modify.bind(null, ((x0 = 1), (function(y) {
+    dec = M.move(zipper.modify.bind(null, ((x0 = 1), (function(y) {
         return (y - x0);
     })))),
-    right = move(zipper.right),
-    left = move(zipper.left),
-    then = (function(a, b) {
-        return (function(z, i) {
-            var __o = a(z, i),
-                zi = __o[0],
-                ii = __o[1];
-            return b(zi, ii);
-        });
-    }),
-    seqa = (function(arr) {
-        return Array.prototype.reduce.call(arr, then);
-    }),
-    seq = (function() {
-        var args = arguments;
-        return seqa(args);
+    right = M.move(zipper.right),
+    left = M.move(zipper.left),
+    set = (function(x) {
+        return M.move(zipper.replace.bind(null, x));
     }),
     loop = (function(body) {
-        return (function loop(z, i) {
-            return ((zipper.extract(z) === 0) ? [z, i] : seq(body, loop)(z, i));
-        });
+        var loop = M.extract.chain((function(x) {
+            return ((x === 0) ? M.of(null) : sequence(body, loop));
+        }));
+        return loop;
     }),
-    get = ((eof = move(zipper.replace.bind(null, 0))), (function(z, i) {
-        return (isEmpty(i) ? eof : [zipper.replace(first(i), z), rest(i)]);
-    })),
-    put = move((function(z) {
-        console.log(fromCharCode(zipper.extract(z)));
-        return z;
+    get = ((eof = set(0)), M.lift(State.get)
+        .chain((function(i) {
+            return (isEmpty(i) ? eof : sequence(set(first(i)), M.lift(M.inner.modify(rest))));
+        }))),
+    put = M.extract.map((function(x) {
+        console.log(fromCharCode(x));
+        return x;
     })),
     semantics = (function(code) {
         return code.map((function(x) {
@@ -95,16 +83,16 @@ var move = (function(op) {
                 case ",":
                     return get;
                 default:
-                    return loop(seqa(semantics(x)));
+                    return loop(sequencea(semantics(x)));
             }
         }));
     }),
     memory = listZipperIn(NIL, 0, repeat(Infinity, 0)),
     exec = (function(program, input) {
-        return program(memory, map(charCode, input));
+        return State.evalState(ZipperT.runZipperT(program, memory), map(charCode, input));
     });
 (evaluateStream = (function(program, input) {
-    return exec(seqa(semantics(parse(program))), input);
+    return exec(sequencea(semantics(parse(program))), input);
 }));
 (evaluate = (function(program, input) {
     return evaluateStream(program, stream.from(input));
